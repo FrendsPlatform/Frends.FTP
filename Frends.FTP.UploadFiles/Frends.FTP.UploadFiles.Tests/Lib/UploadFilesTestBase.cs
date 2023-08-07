74f77c73ba3046f7ab84aa15f439d0c9cbaeba3a
svenskapojkarna <hytonenaleksi@gmail.com>
2023-08-07 14:26:56 +0300

    ISSUE-19: Fixing tests

--- Frends.FTP.UploadFiles/Frends.FTP.UploadFiles.Tests/Lib/UploadFilesTestBase.cs
+++ Frends.FTP.UploadFiles/Frends.FTP.UploadFiles.Tests/Lib/UploadFilesTestBase.cs
@@ -38,27 +38,7 @@ public class UploadFilesTestBase
     public void TearDown()
     {
         if (Directory.Exists(Dir)) Directory.Delete(Dir, true);
-
-        var currentDirectoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
-        var parentDir = currentDirectoryInfo.Parent?.Parent.Parent;
-
-        if (parentDir != null)
-        {
-            var dataFolder = Path.Combine(parentDir.FullName, "DockerVolumes", "data");
-
-            if (Directory.Exists(dataFolder))
-            {
-                var subDirectories = Directory.GetDirectories(dataFolder);
-
-                foreach (var subDirectory in subDirectories)
-                    Directory.Delete(subDirectory, true);
-
-                var files = Directory.GetFiles(dataFolder);
-                foreach (var file in files)
-                    File.Delete(file);
-
-                Console.WriteLine("data-folder cleaned.");
-            }
-        }
+        foreach (var directory in Directory.GetDirectories(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../DockerVolumes/data")))
+            Directory.Delete(directory, true);
     }
 }
