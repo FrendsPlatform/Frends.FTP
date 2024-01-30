52de257b9ab7eedc6b14c7088c96c07702fdf1ec
Riku Virtanen <virtanen.riku91@gmail.com>
2024-01-30 13:17:42 +0200

    Fixing failing test

--- Frends.FTP.DownloadFiles/Frends.FTP.DownloadFiles.Tests/DownloadFilesTests.cs
+++ Frends.FTP.DownloadFiles/Frends.FTP.DownloadFiles.Tests/DownloadFilesTests.cs
@@ -240,6 +240,8 @@ namespace Frends.FTP.DownloadFiles.Tests
         public void DownloadFTP_NoSourceFiles()
         {
             var sourceDir = "ftp";
+            FtpHelper.CreateDirectoryOnFTP(sourceDir);
+            
             var source = new Source { Directory = sourceDir, FileName = "*", Operation = SourceOperation.Nothing, NotFoundAction = SourceNotFoundAction.Error };
             var destination = new Destination { Directory = LocalDirFullPath, Action = DestinationAction.Error };
             var connection = new Connection
@@ -263,6 +265,8 @@ namespace Frends.FTP.DownloadFiles.Tests
             var ex = Assert.Throws<Exception>(() => FTP.DownloadFiles(source, destination, connection, options, new Info(), new CancellationToken()));
             Console.WriteLine(ex.Message);
             Assert.IsTrue(ex.Message.Contains("1 Errors: No source files found from directory"));
+
+            FtpHelper.DeleteDirectoryOnFTP(sourceDir);
         }
     }
 }
